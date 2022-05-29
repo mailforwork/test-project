003f2b1e33a38876378791bb7932d90a49860112
JUHIE CHANDRA <mailforworkjc@gmail.com>
2022-05-29 11:56:38 +0530

    1
    1

--- test/script.js
+++ test/script.js
@@ -43,7 +43,7 @@ function loadLabeledImages() {
         labels.map(async label => {
             const descriptions = []
             for (let i = 1; i <= 2; i++) {
-                const img = await faceapi.fetchImage(`${label}/${i}.jpg`)
+                const img = await faceapi.fetchImage(`../test/labeled_images/${label}/${i}.jpg`)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 descriptions.push(detections.descriptor)
             }
